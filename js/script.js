8a159fcb1e1e7a7cb99bc8b1c28e60ff2c59915d
Sean Ockert <sean.ockert@teachstarter.com>
2020-03-30 09:46:08 +1000

    Add basic graph, global stats, improve filtering

--- js/script.js
+++ js/script.js
@@ -16,29 +16,16 @@ new Vue({
 			'Republic of Korea',
 			'Republic of the Congo',
 			'The Bahamas',
-			'occupied Palestinian territory'
+			'occupied Palestinian territory',
 		],
 		filteredCountries: null,
 		isLoading: false,
 		isIE11: false,
-		months: [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
-			'May',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Oct',
-			'Nov',
-			'Dec'
-		],
-		start: '',
-		end: '',
+		months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		query: '',
+		start: '23 Jan',
 		sort: 'TotalConfirmed',
-		url: 'https://api.covid19api.com/'
+		url: 'https://api.covid19api.com/',
 	},
 	created() {
 		this.init();
@@ -62,25 +49,31 @@ new Vue({
 					this.setActiveCountry();
 				}
 
+				if (localData.ActiveCountryChart) {
+					this.chartPoints = localData.ActiveCountryChart;
+					this.start = localData.ActiveCountryChartStart;
+				}
+
 				var lastFetchTime = new Date(localData.LastFetch).getTime();
 				var last15mins = new Date().getTime() - 1000 * 60 * 15;
 
 				if (lastFetchTime < last15mins) {
 					this.fetchData();
 				} else {
-					// Refresh if last fetch was over 1 hr ago
+					// Refresh if last fetch was over 15mins ago
 					this.setData(localData);
+					this.setActiveCountryStats();
 				}
 			} else {
 				this.fetchData();
 			}
 		},
-		filterCountries(query) {
-			var q = query.split(',');
+		filterCountries() {
+			var q = this.query.split(',');
 
 			this.filteredCountries = this.countries.filter(country => {
 				for (var i = q.length - 1; i >= 0; i--) {
-					if (country.Slug.indexOf(q[i].toLowerCase()) != -1) {
+					if (q[i].trim() != '' && country.Slug.indexOf(q[i].trim().toLowerCase()) != -1) {
 						return country;
 					}
 				}
@@ -99,7 +92,7 @@ new Vue({
 					this.globalStats = {
 						TotalConfirmed: 0,
 						TotalDeaths: 0,
-						TotalRecovered: 0
+						TotalRecovered: 0,
 					};
 
 					data.Countries = data.Countries.filter(country => {
@@ -131,13 +124,25 @@ new Vue({
 					this.setActiveCountry();
 				});
 		},
+		fetchActiveCountryHistory() {
+			fetch(this.url + 'total/country/' + this.activeCountry + '/status/confirmed')
+				.then(res => res.json())
+				.then(data => {
+					if (data.length > 0) {
+						this.buildChart(data);
+
+						var localData = JSON.parse(localStorage.getItem('covid_stats'));
+						localData.ActiveCountryChart = this.chartPoints;
+						localData.ActiveCountryChartStart = this.start;
+						localStorage.setItem('covid_stats', JSON.stringify(localData));
+					}
+				});
+		},
 		setData(data) {
 			this.date = data.Date;
 			this.countries = data.Countries;
 			this.globalStats = data.globalStats;
 			this.filteredCountries = this.countries;
-
-			this.setActiveCountryStats();
 
 			// Sort by URL param
 			var urlParams = new URLSearchParams(window.location.search);
@@ -171,6 +176,7 @@ new Vue({
 						localStorage.setItem('covid_stats', JSON.stringify(localData));
 
 						this.setActiveCountryStats();
+						this.fetchActiveCountryHistory();
 					}
 				});
 		},
@@ -186,62 +192,35 @@ new Vue({
 		},
 		setActiveCountryStats() {
 			// Get your country
-			this.activeCountryStats = this.countries.find(
-				country => country.Slug === this.activeCountry
-			);
-
-			this.buildChart();
+			this.activeCountryStats = this.countries.find(country => country.Slug === this.activeCountry);
 		},
-		buildChart() {
-			if (this.activeCountry != '') {
-				Array.prototype.scaleBetween = function(scaledMin, scaledMax) {
-					var max = Math.max.apply(Math, this);
-					var min = Math.min.apply(Math, this);
-					return this.map(num =>
-						(
-							100 -
-							((scaledMax - scaledMin) * (num - min)) / (max - min) +
-							scaledMin
-						).toFixed(2)
-					);
-				};
+		buildChart(data) {
+			Array.prototype.scaleBetween = function(scaledMin, scaledMax) {
+				var max = Math.max.apply(Math, this);
+				var min = Math.min.apply(Math, this);
+				return this.map(num =>
+					(100 - ((scaledMax - scaledMin) * (num - min)) / (max - min) + scaledMin).toFixed(2)
+				);
+			};
 
-				fetch(
-					this.url + 'total/country/' + this.activeCountry + '/status/confirmed'
-				)
-					.then(res => res.json())
-					.then(data => {
-						if (data.length > 0) {
-							this.chartPoints = '';
+			this.chartPoints = '';
 
-							var start = new Date(data[0].Date);
-							this.start =
-								start.getDate() + ' ' + this.months[start.getMonth()];
+			var start = new Date(data[0].Date);
+			this.start = start.getDate() + ' ' + this.months[start.getMonth()];
 
-							var end = new Date(data[data.length - 1].Date);
-							this.end = end.getDate() + ' ' + this.months[end.getMonth()];
+			/*var end = new Date(data[data.length - 1].Date);
+			this.end = end.getDate() + ' ' + this.months[end.getMonth()];*/
 
-							var y = data.map(point => point.Cases);
-							var scaledY = y.scaleBetween(0, 100);
-							var lenY = scaledY.length;
+			var y = data.map(point => point.Cases);
+			var scaledY = y.scaleBetween(0, 100);
+			var lenY = scaledY.length;
 
-							for (var i = 0; i <= lenY - 1; i++) {
-								this.chartPoints += (i * 500) / lenY + ',' + scaledY[i] + ' ';
-							}
-
-							/*var localData = JSON.parse(localStorage.getItem('covid_stats'));
-							localData.ActiveCountryChart = this.chartPoints;
-							localStorage.setItem('covid_stats', JSON.stringify(localData));
-							*/
-						}
-					});
+			for (var i = 0; i <= lenY - 1; i++) {
+				this.chartPoints += (i * 500) / lenY + ',' + scaledY[i] + ' ';
 			}
 		},
 		scaleBetween(unscaled, minAllowed, maxAllowed, min, max) {
-			return (
-				((maxAllowed - minAllowed) * (unscaled - min)) / (max - min) +
-				minAllowed
-			);
+			return ((maxAllowed - minAllowed) * (unscaled - min)) / (max - min) + minAllowed;
 		},
 		sortBy(field, arr = null) {
 			if (!arr) {
@@ -251,17 +230,11 @@ new Vue({
 			this.sort = field ? field : 'Country';
 
 			arr.sort((a, b) =>
-				b[field] > a[field]
-					? 1
-					: b[field] === a[field]
-					? a.Country > b.Country
-						? 1
-						: -1
-					: -1
+				b[field] > a[field] ? 1 : b[field] === a[field] ? (a.Country > b.Country ? 1 : -1) : -1
 			);
 
 			return arr;
-		}
+		},
 	},
 	filters: {
 		deathsPercent(deaths, total) {
@@ -289,6 +262,6 @@ new Vue({
 				(day_diff < 7 && day_diff + ' days ago') ||
 				(day_diff < 31 && Math.ceil(day_diff / 7) + ' weeks ago')
 			);
-		}
-	}
+		},
+	},
 });
